2b522bedeba3d08cb9f51de28a4a241064a197c4
mackey0209 <seven.km1216@gmail.com>
2020-02-08 05:02:50 +0000

    [add] Mypage (nabe)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,5 +9,11 @@ class CustomersController < ApplicationController
   end
 
   def show
+  	  @customer =Customer.find(params[:id])
+  end
+
+   private
+  def customer_params
+  	  params.require(:customer).permit(:lastname, :firstname, :kana_astname, :kana_firstname, :postal_code, :address, :phone_number, :email, :status)
   end
 end
