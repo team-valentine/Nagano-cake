81b0866263821a2afc51c2fd12c984ba0f8e264a
imarinxx <lotvt.bw.1940@gmail.com>
2020-02-12 07:28:46 +0000

    [edit]items(imarin)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,9 +4,15 @@ class CartItemsController < ApplicationController
   	@cart_items =CartItem.all
   end
 
+  def create
+  	@cart_item = CartItem.new(cart_item_params)
+  	@cart_item.save
+  	redirect_to cart_items_path
+  end
+
   private
   def cart_item_params
-  	params.require(cart_item).permit(:customer,:item,:quantity)
+  	params.require(:cart_item).permit(:customer, :item, :quantity)
   end
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,19 @@
 class ItemsController < ApplicationController
   def index
+  	@genres = Genre.all
+  	if params[:genre_id]
+  		@genre = Genre.find(params[:genre_id])
+  		@items = @genre.items.page(params[:page]).per(8)
+  	else
+  		@items = Item.page(params[:page]).per(8)
+  	end
   end
 
+
   def show
+   	@genres = Genre.all
+  	@item = Item.find(params[:id])
+  	@cart_item =CartItem.new
   end
+
 end
